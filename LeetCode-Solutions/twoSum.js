75fb9f1d056e06793b5ecc89a70755a2f8861243
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-04 10:44:37 -0500

    Update twoSum.js

--- LeetCode-Solutions/twoSum.js
+++ LeetCode-Solutions/twoSum.js
@@ -30,5 +30,21 @@ Constraints:
 -109 <= target <= 109
 Only one valid answer exists.
 
-
 */
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let final = [];
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++ )
+        if(nums[i] + nums[j] === target){
+            final.push(i,j);
+            return final;
+        }
+    }
+};
